9f9e1beca847bcad88493e9fff1b6d59baf51841
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-09 16:14:00 +0545

    feat: registered auth/login route

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from contextlib import asynccontextmanager
 
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
@@ -8,13 +12,11 @@ from src.database import Base
 from src.database import engine
 from src.router import setup_api_routes
 
-from fastapi.staticfiles import StaticFiles
-
 load_dotenv()
 app = FastAPI()
 lifespan = app.router.lifespan_context
+templates = Jinja2Templates(directory="src/templates")
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
-
 
 @asynccontextmanager
 async def lifespan_wrapper(app):
@@ -38,6 +40,15 @@ setup_api_routes(app)
 @app.get("/")
 async def index():
     return {"message": "Hello, world!"}
+
+@app.get("/auth/register")
+def get_register_page(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+
+@app.get("/auth/login")
+def get_login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 
 
 if __name__ == "__main__":
