fad8f0187800098985610d85db62ae7664b0129c
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-12 22:31:57 +0545

    feat: defined the users model

---
+++ app/core/users.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    TIMESTAMP,
+    ForeignKey,
+    JSON,
+    Boolean,
+)
+
+from app.core.database import Base
+
+
+class Role(Base):
+    __tablename__ = "roles"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    permissions = Column(JSON)
+    
+    def __repr__(self):
+        return f"Role(id={self.id}, name={self.name}, permissions={self.permissions})"
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    role_id = Column(Integer, ForeignKey(Role.id))
+    hashed_password: str = Column(String(length=1024), nullable=False)
+    is_active: bool = Column(Boolean, default=True, nullable=False)
+    is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
+    
+    def __repr__(self):
+        return f"User(id={self.id}, email={self.email}, username={self.username}, role_id={self.role_id}, is_active={self.is_active}, is_superuser={self.is_superuser}, is_verified={self.is_verified})"
