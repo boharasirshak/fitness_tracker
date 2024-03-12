674b10dc717cfe00816ccc49cef41388dfc30450
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-12 22:20:18 +0545

    feat: defined api/v1/

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 # Static Files
-# app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # Templates
-# templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/")
 async def read_root():
