48a3ff53cbdc634e0d52aa28ee6cf319d9e34702
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-13 16:01:33 +0545

    feat: updated config file as per .env

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 from datetime import timedelta
 
+import aiosmtplib
 from dotenv import load_dotenv
 from fastapi_jwt import (
     JwtAccessBearerCookie,
@@ -9,11 +10,20 @@ from fastapi_jwt import (
 
 load_dotenv()
 
+BASE_URL = os.environ.get("BASE_URL")
+
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
+
+SMTP_HOST = os.environ.get("SMTP_HOST")
+SMTP_USER = os.environ.get("SMTP_USER")
+SMTP_PASS = os.environ.get("SMTP_PASS")
+SMTP_PORT = os.environ.get("SMTP_PORT") or 587
+SMTP_FROM = os.environ.get("SMTP_FROM") or SMTP_USER
+SMTP_FROM_NAME = os.environ.get("SMTP_FROM_NAME") or SMTP_FROM
 
 ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET") or "FITNESS_SECRET_TOKEN"
 REFRESH_TOKEN_SECRET = os.getenv("REFRESH_TOKEN_SECRET") or "FITNESS_REFRESH_TOKEN"
@@ -29,4 +39,10 @@ access_security = JwtAccessBearerCookie(
 refresh_security = JwtRefreshBearerCookie(
     auto_error=True,
     secret_key=REFRESH_TOKEN_SECRET,
+)
+
+smtp = aiosmtplib.SMTP(
+    hostname=SMTP_HOST,
+    port=SMTP_PORT,
+    start_tls=False
 )
