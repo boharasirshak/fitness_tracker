0c83a331b0e01263e19b43ec7a645aeef4676a74
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-13 11:26:16 +0545

    feat: main file

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
+import asyncio
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+from app.core.database import create_tables
+from app.api.v1.auth import router as auth_router
 
-# Static Files
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Startup code")
+    asyncio.create_task(create_tables())
+    yield
+
+app = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
-# Templates
 templates = Jinja2Templates(directory="app/templates")
+
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+app.include_router(auth_router, prefix="/v1")
