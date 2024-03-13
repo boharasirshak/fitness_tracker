ecf2457fc9590961feca1cf4121d17b06561836e
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-13 16:05:45 +0545

    feat: create table on startup and rename routes to /api/v1/

--- app/main.py
+++ app/main.py
@@ -1,18 +1,25 @@
 import asyncio
+import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from app.core.database import create_tables
 from app.api.v1.auth import router as auth_router
+from app.core.emails import init_smtp
+from app.core.database import create_tables, drop_tables
+
+logging.getLogger('passlib').setLevel(logging.ERROR)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("Startup code")
+    # Warning: This will drop all tables, so use only in dev
+    # asyncio.create_task(drop_tables())
     asyncio.create_task(create_tables())
+    asyncio.create_task(init_smtp())
+    print("Setup SMTP and Database Tables Successful")
     yield
 
 app = FastAPI(lifespan=lifespan)
@@ -24,4 +31,4 @@ templates = Jinja2Templates(directory="app/templates")
 async def read_root():
     return {"Hello": "World"}
 
-app.include_router(auth_router, prefix="/v1")
+app.include_router(auth_router, prefix="/api/v1")
