5442f74623b1b5fe5cf91e5c8233671525c85cf7
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-14 23:07:20 +0545

    feat: workouts model

---
+++ app/models/workouts.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    Column,
+    Integer,
+    TIMESTAMP,
+    ForeignKey,
+)
+from app.core.database import Base
+
+
+class Workout(Base):
+    __tablename__ = "workouts"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    exercise_id = Column(Integer, ForeignKey("exercises.id"), nullable=False, unique=True)
+
+    # These two data will be calculated by the neural network
+
+    repetitions = Column(Integer, nullable=False)  # The number of repetitions the user did
+    # sets = Column(Integer, nullable=False) # The sets of the exercise the user did
+
+    created_at = Column(TIMESTAMP, nullable=False, default=datetime.utcnow)
+
+    # Relationship
+    exercise = relationship("Exercise", back_populates="workouts")
