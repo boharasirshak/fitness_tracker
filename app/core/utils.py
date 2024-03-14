ebf125c7ab10204c5182ddeb06c09c28e4446812
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-14 12:26:43 +0545

    feat: do not use punctuations during generating random passwords

--- app/core/utils.py
+++ app/core/utils.py
@@ -3,5 +3,5 @@ import random
 
 
 def generate_random_password(length: int):
-    charset = string.ascii_letters + string.digits + string.punctuation
+    charset = string.ascii_letters + string.digits
     return "".join(random.choices(charset, k=length))
