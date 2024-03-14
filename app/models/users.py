6472020d129f9957dbf5f93b22015c4d8470be76
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-14 14:08:44 +0545

    feat: the users table's activity_level is Enum

--- app/models/users.py
+++ app/models/users.py
@@ -1,3 +1,4 @@
+import enum
 from datetime import datetime
 
 from sqlalchemy import (
@@ -10,9 +11,17 @@ from sqlalchemy import (
 from app.core.database import Base
 
 
+class ActivityLevel(enum.Enum):
+    NOT_GIVEN = 0  # Value not given during registration
+    BEGINNER = 1  # Doesn't practice much
+    AVERAGE = 2  # Practice sports 1-3 timer per week
+    ATHLETE = 3  # Practice sports 3-5 times per week
+    # add more activity levels here
+
+
 class User(Base):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String, nullable=False)
     username = Column(String, nullable=True, default="")
@@ -20,5 +29,5 @@ class User(Base):
     gender = Column(String, nullable=True, default="")
     height = Column(Integer, nullable=True, default=0)
     weight = Column(Integer, nullable=True, default=0)
-    activity_level = Column(Integer, nullable=True, default=0)
+    activity_level = Column(Integer, create_type=False, nullable=True, default=ActivityLevel.NOT_GIVEN.value)
     registered_at = Column(TIMESTAMP, default=datetime.utcnow)
