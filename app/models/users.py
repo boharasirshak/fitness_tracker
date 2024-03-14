38d4ee5a821e42f4869b3499202fe30ef3f503a6
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-14 12:25:55 +0545

    feat: modified the users table and auth schema to better suite the application logic

--- app/models/users.py
+++ app/models/users.py
@@ -5,7 +5,6 @@ from sqlalchemy import (
     Integer,
     String,
     TIMESTAMP,
-    Boolean,
 )
 
 from app.core.database import Base
@@ -16,13 +15,10 @@ class User(Base):
     
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String, nullable=False)
-    username = Column(String, nullable=False, default="")
+    username = Column(String, nullable=True, default="")
+    hashed_password: str = Column(String, nullable=False)
+    gender = Column(String, nullable=True, default="")
+    height = Column(Integer, nullable=True, default=0)
+    weight = Column(Integer, nullable=True, default=0)
+    activity_level = Column(Integer, nullable=True, default=0)
     registered_at = Column(TIMESTAMP, default=datetime.utcnow)
-    hashed_password: str = Column(String(length=1024), nullable=False)
-    is_active: bool = Column(Boolean, default=True, nullable=False)
-    is_superuser: bool = Column(Boolean, default=False, nullable=False)
-    is_verified: bool = Column(Boolean, default=False, nullable=False)
-    
-    def __repr__(self):
-        return (f"User(id={self.id}, email={self.email}, username={self.username}, role_id={self.role_id}, "
-                f"is_active={self.is_active}, is_superuser={self.is_superuser}, is_verified={self.is_verified})")
