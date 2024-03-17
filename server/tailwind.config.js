3c674cec3e1a76b48d3fb8b698c332dfe8ea0640
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-18 02:09:31 +0545

    feat: tailwind

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["src/templates/*.html"],
+  content: ["app/templates/**/*.html"],
   darkMode: 'class',
   theme: {
     extend: {
