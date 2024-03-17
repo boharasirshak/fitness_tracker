88031486afeea00bd95ff37a12c72602ab6d09d4
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-17 15:30:32 +0545

    feat: setup docker

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, loadEnv } from 'vite';
 
 export default () => {
-  process.env = {...process.env, ...loadEnv('public', process.cwd())};
+  process.env = {...process.env, ...loadEnv('', process.cwd())};
 
   return defineConfig({
     plugins: [sveltekit()],
