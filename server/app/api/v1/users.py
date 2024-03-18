c07b65eeb6ba86adb5ae3aa582bfe9cff74a8552
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-19 02:35:31 +0545

    feat: use db directly instead of auto-quitting session along with naming changes.

--- server/app/api/v1/users.py
+++ server/app/api/v1/users.py
@@ -3,10 +3,7 @@ import enum
 from sqlalchemy.sql import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from fastapi import APIRouter, Depends, Security
-from fastapi_jwt import JwtAuthorizationCredentials
+from fastapi import APIRouter, Depends
 
 from app.dependencies.jwt import jwt_verify
 from app.models.users import User
@@ -14,10 +11,7 @@ from app.core.database import get_db
 from app.schemas import ErrorResponseSchema
 from app.schemas.users import (
     UserDataUpdateSchema,
-    UserDataSchema
-)
-from app.config import (
-    access_security
+    UserSchema
 )
 
 router = APIRouter(prefix="/users", tags=["Пользователи"])
@@ -27,21 +21,23 @@ router = APIRouter(prefix="/users", tags=["Пользователи"])
     "/me",
     response_description="Gets user's data",
     responses={
-        200: {"model": UserDataSchema, "description": "Все данные пользователя"},
-        401: {"model": ErrorResponseSchema, "description": "Токен недействителен, срок действия истек или не предоставлен"},
+        200: {"model": UserSchema, "description": "Все данные пользователя"},
+        401: {"model": ErrorResponseSchema,
+              "description": "Токен недействителен, срок действия истек или не предоставлен"},
         404: {"model": ErrorResponseSchema, "description": "Пользователь не найден"},
     }
 )
 async def get_user_data(
     user: User = Depends(jwt_verify)
 ):
-    return UserDataSchema(
+    return UserSchema(
         email=user.email,
         username=user.username,
         gender=user.gender,
         height=user.height,
         weight=user.weight,
-        activity_level=user.activity_level
+        activity_level=user.activity_level,
+        phone_number=user.phone_number or ""
     )
 
 
@@ -49,8 +45,9 @@ async def get_user_data(
     "/",
     response_description="Обновляет данные пользователя",
     responses={
-        200: {"model": UserDataSchema, "description": "Все обновленные данные пользователя"},
-        401: {"model": ErrorResponseSchema, "description": "Токен недействителен, срок действия истек или не предоставлен"},
+        200: {"model": UserSchema, "description": "Все обновленные данные пользователя"},
+        401: {"model": ErrorResponseSchema,
+              "description": "Токен недействителен, срок действия истек или не предоставлен"},
         404: {"model": ErrorResponseSchema, "description": "Пользователь не найден"},
     }
 )
@@ -59,27 +56,26 @@ async def change_user_data(
         db: AsyncSession = Depends(get_db),
         user: User = Depends(jwt_verify)
 ):
-    async with db as session:
-        # we create a new user object because the `user` we get from jwt_verify is not persistent in the session
-        # noinspection PyTypeChecker
-        query = select(User).where(User.id == user.id)
-        result = await session.execute(query)
-        new_user = result.scalar()
+    # noinspection PyTypeChecker
+    query = select(User).where(User.id == user.id)
+    result = await db.execute(query)
+    new_user = result.scalar()
 
-        update_data = data.model_dump(exclude_none=True)
-        for key, value in update_data.items():
-            if isinstance(value, enum.Enum):
-                value = value.value
-            setattr(new_user, key, value)
+    update_data = data.model_dump(exclude_none=True)
+    for key, value in update_data.items():
+        if isinstance(value, enum.Enum):
+            value = value.value
+        setattr(new_user, key, value)
 
-        await session.commit()
-        await session.refresh(new_user)
+    await db.commit()
+    await db.refresh(new_user)
 
-        return UserDataSchema(
-            email=new_user.email,
-            username=new_user.username,
-            gender=new_user.gender,
-            height=new_user.height,
-            weight=new_user.weight,
-            activity_level=new_user.activity_level
-        )
+    return UserSchema(
+        email=new_user.email,
+        username=new_user.username,
+        gender=new_user.gender,
+        height=new_user.height,
+        weight=new_user.weight,
+        activity_level=new_user.activity_level,
+        phone_number=new_user.phone_number
+    )
