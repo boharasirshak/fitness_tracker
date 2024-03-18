b140df0f7bfb2e92aedcc22e54509614c03aaa9c
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-19 02:41:45 +0545

    refactor: added the newly added routes

--- server/app/main.py
+++ server/app/main.py
@@ -11,6 +11,8 @@ from app.core.emails import init_smtp
 from app.api.v1.auth import router as auth_router
 from app.api.v1.users import router as users_router
 from app.api.v1.tokens import router as tokens_router
+from app.api.v1.exercises import router as exercises_router
+from app.api.v1.workouts import router as workouts_router
 
 from app.core.database import (
     create_tables,
@@ -26,24 +28,27 @@ async def lifespan(_: FastAPI):
     # asyncio.create_task(drop_tables())
     asyncio.create_task(init_smtp())
     asyncio.create_task(create_tables())
-    print("Setup Successful")
     yield
+
 
 app = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
+# noinspection PyTypeChecker
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.get("/register")
 def get_register_page(request: Request):
@@ -54,18 +59,24 @@ def get_register_page(request: Request):
 def get_login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
+
 @app.get("/dashboard")
 def get_login_page(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request})
+
 
 @app.get("/profile")
 def get_login_page(request: Request):
     return templates.TemplateResponse("profile.html", {"request": request})
 
+
 @app.get("/workouts/new")
 def get_login_page(request: Request):
     return templates.TemplateResponse("new-workouts.html", {"request": request})
 
+
 app.include_router(auth_router, prefix="/api/v1")
 app.include_router(users_router, prefix="/api/v1")
 app.include_router(tokens_router, prefix="/api/v1")
+app.include_router(exercises_router, prefix="/api/v1")
+app.include_router(workouts_router, prefix="/api/v1")
