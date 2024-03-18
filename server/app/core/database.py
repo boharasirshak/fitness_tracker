1a52037861cd82c6e4c43461eddbb729798d3bee
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-19 02:40:46 +0545

    refactor: use connection pooling

--- server/app/core/database.py
+++ server/app/core/database.py
@@ -1,14 +1,21 @@
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from app.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    pool_size=2,
+    max_overflow=0,
+    pool_recycle=1800,
+    pool_timeout=30,
+)
 
 # noinspection PyTypeChecker
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
