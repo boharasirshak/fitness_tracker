de89329f75b29ee845b4364b2a1609aecd39db72
Sirshak Bohara <boharasirshak101@gmail.com>
2024-04-01 10:52:50 +0545

    feat: logs off

--- server/app/core/database.py
+++ server/app/core/database.py
@@ -10,16 +10,12 @@ DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(
     DATABASE_URL,
-    echo=True,
+    echo=False,
     future=True,
     pool_pre_ping=True,
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False, 
-    autoflush=False, 
-    bind=engine 
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
