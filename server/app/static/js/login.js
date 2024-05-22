42d6c9c355f0750af2dfcc8139f312eb0f10cac8
Sirshak Bohara <boharasirshak101@gmail.com>
2024-05-22 09:35:43 +0545

    feat: completed login page

--- server/app/static/js/login.js
+++ server/app/static/js/login.js
@@ -1,49 +1,52 @@
-document.getElementById('submit')?.addEventListener('click', async e => {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+document.getElementById("submit")?.addEventListener("click", async (e) => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-    if (!email || !password) {
-        document.getElementById('toast-success').style.display = 'none';
-        document.getElementById('toast-danger').style.display = '';
-        document.getElementById('toast-danger-text').innerText = 'Требуется адрес электронной почты и пароль!';
-        return;
+  if (!email || !password) {
+    document.getElementById("toast-success").style.display = "none";
+    document.getElementById("toast-danger").style.display = "";
+    document.getElementById("toast-danger-text").innerText =
+      "Требуется адрес электронной почты и пароль!";
+    return;
+  }
+
+  const response = await fetch("/api/v1/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  try {
+    const data = await response.json();
+    if (response.status >= 400) {
+      console.log(response.status, data.detail);
+      document.getElementById("toast-success").style.display = "none";
+      document.getElementById("toast-danger").style.display = "";
+      if (typeof data.detail === "string") {
+        document.getElementById("toast-danger-text").innerText = data.detail;
+      } else {
+        // get the first error of the FastAPI 422 validation error
+        document.getElementById("toast-danger-text").innerText =
+          data.detail[0].msg;
+      }
+      return;
     }
 
-    const response = await fetch('/api/v1/auth/login', {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-    });
+    document.getElementById("toast-danger").style.display = "none";
+    document.getElementById("toast-success").style.display = "";
+    document.getElementById("toast-success-text").innerText =
+      "Пользователь успешно вошел в систему. Перенаправление на панель мониторинга...";
 
-    try {
-        const data = await response.json();
-        if (response.status >= 400) {
-            console.log(response.status, response.data)
-            document.getElementById('toast-success').style.display = 'none';
-            document.getElementById('toast-danger').style.display = '';
-            if (typeof data.detail === 'string') {
-                document.getElementById('toast-danger-text').innerText = data.detail;
-            } else {
-                // get the first error of the FastAPI 422 validation error
-                document.getElementById('toast-danger-text').innerText = data.detail[0].msg;
-            }
-            return;
-        }
-    
-        document.getElementById('toast-danger').style.display = 'none';
-        document.getElementById('toast-success').style.display = '';
-        document.getElementById('toast-success-text').innerText = "Пользователь успешно вошел в систему. Перенаправление на панель мониторинга...";
-    
-        localStorage.setItem('access_token', data.access_token);
-        localStorage.setItem('refresh_token', data.refresh_token);
-    
-        window.location.href = '/dashboard';
+    localStorage.setItem("access_token", data.access_token);
+    localStorage.setItem("refresh_token", data.refresh_token);
 
-    } catch {
-        document.getElementById('toast-success').style.display = 'none';
-        document.getElementById('toast-danger').style.display = '';
-        document.getElementById('toast-danger-text').innerText = 'Внутренняя ошибка сервера. Пожалуйста, повторите попытку позже.';
-    }
+    window.location.href = "/dashboard";
+  } catch {
+    document.getElementById("toast-success").style.display = "none";
+    document.getElementById("toast-danger").style.display = "";
+    document.getElementById("toast-danger-text").innerText =
+      "Внутренняя ошибка сервера. Пожалуйста, повторите попытку позже.";
+  }
 });
