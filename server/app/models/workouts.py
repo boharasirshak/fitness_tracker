c01bc913edf70adc1982b7b6042e36343da55464
Sirshak Bohara <boharasirshak101@gmail.com>
2024-05-28 01:03:09 +0545

    feat: removed unused table columns

--- server/app/models/workouts.py
+++ server/app/models/workouts.py
@@ -1,10 +1,4 @@
-from sqlalchemy import (
-    Column,
-    Integer,
-    String,
-    DateTime,
-    ForeignKey
-)
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.sql import func
 
 from app.core.database import Base
@@ -16,9 +10,6 @@ class Workout(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
-    total_time = Column(Integer, nullable=False)
-    rest_time = Column(Integer, nullable=False)
     efficiency = Column(Integer, nullable=False, default=0)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    exercise_id = Column(String, ForeignKey("exercises.id"), nullable=False)
     created_at = Column(DateTime(timezone=True), default=func.now())
