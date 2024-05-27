c3d9a6f0a09e15ea0c35c2ef5fa47123dc97fa31
Sirshak Bohara <boharasirshak101@gmail.com>
2024-05-28 01:03:21 +0545

    feat: import new things

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -3,4 +3,4 @@ from .exercises import Exercise
 from .workouts import Workout
 from .workout_exercises import WorkoutExercise
 from .workout_sessions import WorkoutSession
-from .auth_tokens import AuthToken
+from .reset_password_token import ResetPasswordToken
