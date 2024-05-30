6e347b774333e344d16aa97e8373859c699d3cbc
Sirshak Bohara <boharasirshak101@gmail.com>
2024-05-30 12:35:50 +0545

    feat: semi-working profile page

--- server/app/main.py
+++ server/app/main.py
@@ -1,17 +1,24 @@
+import json
 import asyncio
 import logging
-from pathlib import Path
+from datetime import datetime, timedelta
 from contextlib import asynccontextmanager
 
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql import select
+from sqlalchemy.orm import joinedload
+
 # from app.core.emails import init_smtp
-from app.models.workouts import Workout
+from app.models import Workout, WorkoutExercise, User
+from app.schemas.workouts import workout_to_schema
 from app.core.utils import insert_default_data
-from app.dependencies.workouts import workout_verify
+from app.core.security import is_valid_jwt
 
 from app.api.v1.auth import router as auth_router
 from app.api.v1.users import router as users_router
@@ -20,7 +27,8 @@ from app.api.v1.exercises import router as exercises_router
 from app.api.v1.websockets import router as websockets_router
 from app.api.v1.workouts import router as workouts_router
 
-from app.core.database import create_tables, drop_tables
+from app.core.database import create_tables, drop_tables, get_db
+from app.schemas.users import UserSchema
 
 logging.getLogger("passlib").setLevel(logging.ERROR)
 
@@ -51,46 +59,152 @@ app.add_middleware(
 
 @app.get("/")
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    # return templates.TemplateResponse("index.html", {"request": request})
+    return RedirectResponse("/login")
 
 
 @app.get("/register")
 def register_page(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request})
+    access_token = request.cookies.get("access_token")
+    if access_token and is_valid_jwt(access_token):
+        return RedirectResponse(url="/dashboard")
+
+    response = templates.TemplateResponse(
+        "register.html",
+        {
+            "request": request,
+        },
+    )
+    if access_token:
+        response.delete_cookie("access_token")
+
+    return response
 
 
 @app.get("/login")
 def login_page(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
+    access_token = request.cookies.get("access_token")
+
+    if access_token and is_valid_jwt(access_token):
+        return RedirectResponse(url="/dashboard")
+
+    response = templates.TemplateResponse(
+        "login.html",
+        {
+            "request": request,
+        },
+    )
+    if access_token:
+        response.delete_cookie("access_token")
+
+    return response
 
 
 @app.get("/dashboard")
-def dashboard_page(request: Request):
-    return templates.TemplateResponse("dashboard.html", {"request": request})
+async def dashboard_page(request: Request, db: AsyncSession = Depends(get_db)):
+    access_token = request.cookies.get("access_token")
+    if not access_token:
+        return RedirectResponse(url="/login")
+
+    token = is_valid_jwt(access_token)
+
+    if not token or token.get("subject", {}).get("user_id") is None:
+        resp = RedirectResponse(url="/login")
+        resp.delete_cookie("access_token")
+        return resp
+
+    query = (
+        select(Workout)
+        .where(Workout.user_id == token["subject"]["user_id"])
+        .options(
+            joinedload(Workout.user),
+            joinedload(Workout.workout_exercises).joinedload(WorkoutExercise.exercise),
+            joinedload(Workout.workout_exercises).joinedload(
+                WorkoutExercise.workout_sessions
+            ),
+        )
+    )
+
+    workouts = []
+    result = await db.execute(query)
+    data = result.unique().scalars().all()
+
+    for workout in data:
+        workouts.append(json.loads(workout_to_schema(workout).model_dump_json()))
+
+    return templates.TemplateResponse(
+        "dashboard.html",
+        {
+            "request": request,
+            "access_token": access_token,
+            "workouts": workouts,
+        },
+    )
 
 
 @app.get("/profile")
-def profile_page(request: Request):
-    return templates.TemplateResponse("profile.html", {"request": request})
+async def profile_page(request: Request, db: AsyncSession = Depends(get_db)):
+    access_token = request.cookies.get("access_token")
+    if not access_token:
+        return RedirectResponse(url="/login")
 
+    token = is_valid_jwt(access_token)
 
-@app.get("/workouts/")
-def new_workout(request: Request):
-    return templates.TemplateResponse("workouts.html", {"request": request})
+    if not token or token.get("subject", {}).get("user_id") is None:
+        resp = RedirectResponse(url="/login")
+        resp.delete_cookie("access_token")
+        return resp
 
+    query = select(User).where(User.id == token["subject"]["user_id"])
+    result = await db.execute(query)
+    user = result.scalar()
 
-@app.get("/workouts/new")
-def new_workout(request: Request):
-    return templates.TemplateResponse("new-workout.html", {"request": request})
+    if not user:
+        resp = RedirectResponse(url="/login")
+        resp.delete_cookie("access_token")
+        return resp
 
-
-@app.get("/workouts/start/{workout_id}")
-def start_workout(
-    workout_id: int, request: Request, workout: Workout = Depends(workout_verify)
-):
     return templates.TemplateResponse(
-        "workout.html", {"request": request, "workout": workout}
+        "profile.html",
+        {
+            "request": request,
+            "access_token": access_token,
+            "user": UserSchema(
+                email=user.email,
+                name=user.name,
+                gender=user.gender,
+                height=user.height,
+                weight=user.weight,
+                activity_level=user.activity_level,
+                profile_picture_url=user.profile_picture_url or "",
+                age=user.age,
+                desired_weight=user.desired_weight,
+            ),
+        },
     )
+
+
+# @app.get("/workouts/")
+# def new_workout(request: Request):
+#     access_token = request.cookies.get("access_token", None)
+#     return templates.TemplateResponse(
+#         "workouts.html", {"request": request, "access_token": access_token}
+#     )
+
+
+# @app.get("/workouts/new")
+# def new_workout(request: Request):
+#     access_token = request.cookies.get("access_token", None)
+#     return templates.TemplateResponse("new-workout.html.html", {"request": request, "access_token": access_token})
+
+
+# @app.get("/workouts/start/{workout_id}")
+# def start_workout(
+#     workout_id: int, request: Request, workout: Workout = Depends(workout_verify)
+# ):
+#     return templates.TemplateResponse(
+#         "workout.html", {"request": request, "workout": workout}
+#     )
 
 
 app.include_router(auth_router, prefix="/api/v1")
