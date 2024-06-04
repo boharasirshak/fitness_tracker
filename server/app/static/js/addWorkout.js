97b5e0ce7d261afdd86b4d2dfbfa74d6fddc29c2
Sirshak Bohara <boharasirshak101@gmail.com>
2024-06-05 04:26:27 +0545

    feat: static add exercise page

--- server/app/static/js/addWorkout.js
+++ server/app/static/js/addWorkout.js
@@ -1,141 +1,197 @@
 let customExercises = [];
-let workoutName = "";
-const workoutPage = document.getElementById("workout-page");
-const editWorkoutPage = document.getElementById("edit-workout-page");
+let editWorkoutPage = document.getElementById("edit-workout-page");
+let editBox = document.getElementById("edit-box");
+let workoutContainer = document.getElementById("workout-container");
 
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (mutation.type === "attributes" && mutation.attributeName === "style") {
-      const displayValue = workoutPage.style.display;
-
-      console.log(`Display value: ${displayValue}`);
-
-      // CASE: the edit workout page is displayed
-      if (displayValue === "none") {
-        if (customExercises.length > 0) {
-          const editBox = document.getElementById("edit-box");
-          editBox.innerHTML = "";
-          const title = document.createElement("h5");
-          title.textContent = "Список упражнений";
-          editBox.appendChild(title);
-
-          for (const exercise of customExercises) {
-            const timerEdit = document.createElement("div");
-            timerEdit.className = "timer-edit";
-            timerEdit.innerHTML = `
-              <div class="squat-wrap">
-                <img src="../static/images/squat.png" alt="" />
-              </div>
-              <h3>${exercise.name}</h3>
-              <div class="timer-wrap">
-                <span>0:10</span>
-              </div>
-            `;
-            editBox.appendChild(timerEdit);
-          }
-
-          editBox.innerHTML += `
-            <div class="flex align-center gap-20 btns-wrap">
-              <button class="white-btn">
-                <img src="../static/images/plus.svg" alt="" />Отдых
-              </button>
-              <button class="white-btn" onclick="showPage('workout-page')">
-                <img src="../static/images/plus.svg" alt="" />Упражнение
-              </button>
-            </div>
-          `;
-        } else {
-          document.getElementById("edit-box").innerHTML = `
-          <h5>Список упражнений</h5>
-              <div class="text-edit-wrap">
-                <div
-                  class="list-exercise"
-                  id="add-exercise-btn"
-                  style="cursor: pointer"
-                  onclick="showPage('workout-page')"
-                >
-                  <img src="../static/images/plus.svg" alt="" />
-                  Упражнение
-                </div>
-              </div>
-          `;
-        }
-
-        // CASE: the choose workouts page is displayed
-      } else {
-        let workoutContainer = document.getElementById("workout-container");
-        workoutContainer.innerHTML = "";
-        for (const exercise of exercises) {
-          const element = document.createElement("div");
-          element.className = "workout-card new";
-          element.innerHTML = `
-          <img
-              src="../static/images/exercise-1.png"
-              alt=""
-              class="exercise-img"
-            />
-            <h4>${exercise.name}</h4>
-            <img src="../static/images/add-btn.svg" alt="" style="cursor: pointer;" />
-            <div class="exercise-popup-wraps">
-              <div class="exercise-pop-wrapper">
-                <h4>${exercise.name}</h4>
-                <div class="workout">
-                  <img src="../static/images/exercise-1.png" alt="" />
-                </div>
-                <div class="timer-count">
-                  <div class="plus-minus">
-                    <img src="../static/images/minus.svg" alt="" />
-                  </div>
-                  <div class="timer-wraps">
-                    <span>0:10</span>
-                  </div>
-                  <div class="plus-minus">
-                    <img src="../static/images/plus.svg" alt="" />
-                  </div>
-                </div>
-                <div class="flex align-center add-flex gap-20">
-                  <button class="btn" onclick='addNewExercise({
-                    "name": "${exercise.name}",
-                    "id": "${exercise.id}",
-                    "gif_link": "${exercise.gif_link}",
-                    "video_link": "${exercise.video_link}",
-                  })'>Добавить</button>
-                  <div class="cross">
-                    <img src="../static/images/cross.svg" alt="" />
-                  </div>
-                </div>
-            </div>
-          `;
-          workoutContainer.appendChild(element);
-        }
-
-        const workoutCards = document.querySelectorAll(".workout-card");
-        const closeBtns = document.querySelectorAll(".cross");
-
-        workoutCards.forEach(function (card) {
-          card.addEventListener("click", function () {
-            const exercisePopup = this.querySelector(".exercise-popup-wraps");
-            exercisePopup.classList.add("active");
-          });
-        });
-
-        closeBtns.forEach(function (btn) {
-          btn.addEventListener("click", function (event) {
-            const exercisePopup = this.closest(".exercise-popup-wraps");
-            exercisePopup.classList.remove("active");
-            event.stopPropagation();
-          });
-        });
-      }
-    }
-  });
-});
-
-observer.observe(workoutPage, { attributes: true, attributeFilter: ["style"] });
-observer.observe(editWorkoutPage, {
+const observer = new MutationObserver(updateUI);
+observer.observe(document.getElementById("workout-page"), {
   attributes: true,
   attributeFilter: ["style"],
 });
+observer.observe(document.getElementById("edit-workout-page"), {
+  attributes: true,
+  attributeFilter: ["style"],
+});
+
+function updateUI(mutations) {
+  mutations.forEach((mutation) => {
+    if (mutation.type === "attributes" && mutation.attributeName === "style") {
+      if (editWorkoutPage.style.display === "none") {
+        workoutContainer.innerHTML = "";
+        exercises.forEach((exercise) => {
+          createWorkoutCard(exercise);
+        });
+      } else {
+        editBox.innerHTML = "";
+        editBox.appendChild(createExerciseList());
+      }
+    }
+  });
+}
+
+function createWorkoutCard(exercise) {
+  const card = document.createElement("div");
+  card.className = "workout-card new";
+  card.innerHTML = `
+  <img
+      src="../static/images/exercise-1.png"
+      alt=""
+      class="exercise-img"
+    />
+    <h4>${exercise.name}</h4>
+    <img src="../static/images/add-btn.svg" alt="" style="cursor: pointer;" />
+    <div class="exercise-popup-wraps">
+      <div class="exercise-pop-wrapper">
+        <h4>${exercise.name}</h4>
+        <div class="workout">
+          <img src="../static/images/exercise-1.png" alt="" />
+        </div>
+        <div class="timer-count">
+          <div class="plus-minus" type="minus" style="cursor: pointer;">
+            <img src="../static/images/minus.svg" alt="" />
+          </div>
+          <div class="timer-wraps" type="timer">
+            <input value="10" type="timer"/>
+          </div>
+          <div class="plus-minus" type="plus" style="cursor: pointer;">
+            <img src="../static/images/plus.svg" alt="" />
+          </div>
+        </div>
+        <div class="flex align-center add-flex gap-20">
+          <button class="btn" type="add" >Добавить</button>
+          <div class="cross">
+            <img src="../static/images/cross.svg" alt="" />
+          </div>
+        </div>
+    </div>
+  `;
+
+  workoutContainer.appendChild(card);
+
+  let timer = 10;
+  const exercisePopup = card.querySelector(".exercise-popup-wraps");
+  const closeBtn = card.querySelector(".cross");
+  const plusBtn = card.querySelector(".plus-minus[type='plus']");
+  const minusBtn = card.querySelector(".plus-minus[type='minus']");
+  const timerInput = card.querySelector("input[type='timer']");
+  const addBtn = card.querySelector(".btn[type='add']");
+
+  card.addEventListener("click", () => exercisePopup.classList.add("active"));
+  closeBtn.addEventListener("click", (event) => {
+    exercisePopup.classList.remove("active");
+    event.stopPropagation();
+  });
+
+  plusBtn.addEventListener("click", () => {
+    timer += 1;
+    timerInput.value = timer;
+  });
+
+  minusBtn.addEventListener("click", () => {
+    if (timer <= 1) return;
+    timer -= 1;
+    timerInput.value = timer;
+  });
+
+  addBtn.addEventListener("click", () => {
+    const newExercise = {
+      name: exercise.name,
+      id: exercise.id,
+      gif_link: exercise.gif_link,
+      video_link: exercise.video_link,
+      duration: timer,
+      rest_timeout: 0,
+    };
+    customExercises.push(newExercise);
+    console.log(customExercises);
+  });
+}
+
+function createExerciseList() {
+  const list = document.createElement("div");
+  const title = document.createElement("h5");
+  title.textContent = "Список упражнений";
+  list.appendChild(title);
+
+  if (customExercises.length > 0) {
+    customExercises.forEach((exercise) => {
+      const exerciseElement = document.createElement("div");
+      exerciseElement.className = "timer-edit";
+      exerciseElement.innerHTML = `
+      <div class="squat-wrap">
+        <img src="../static/images/squat.png" alt="" />
+      </div>
+      <h3>${exercise.name}</h3>
+      <div class="timer-wrap">
+        <input value="${exercise.duration}" />
+      </div>
+      `;
+      list.appendChild(exerciseElement);
+
+      if (exercise.rest_timeout > 0) {
+        const restElement = document.createElement("div");
+        restElement.className = "timer-edit";
+        restElement.innerHTML = `
+        <div class="center-wrap">
+          <img src="../static/images/dots.svg" alt="" />
+          </div>
+          <h3>Отдых</h3>
+          <div class="timer-wrap">
+            <input value="${exercise.rest_timeout}" />
+          </div>
+        `;
+        list.appendChild(restElement);
+      }
+    });
+    editBox.appendChild(createButtons());
+  } else {
+    const container = document.createElement("div");
+    container.className = "text-edit-wrap";
+
+    const addExerciseBtn = document.createElement("div");
+    addExerciseBtn.className = "list-exercise";
+    addExerciseBtn.id = "add-exercise-btn";
+    addExerciseBtn.style.cursor = "pointer";
+    addExerciseBtn.onclick = () => showPage("workout-page");
+    addExerciseBtn.innerHTML =
+      "<img src='../static/images/plus.svg' alt='' /> Упражнение";
+    container.appendChild(addExerciseBtn);
+    list.appendChild(container);
+  }
+
+  return list;
+}
+
+function createButtons() {
+  const buttonsWrapper = document.createElement("div");
+  buttonsWrapper.className = "flex align-center gap-20 btns-wrap";
+
+  const restBtn = document.createElement("button");
+  restBtn.className = "white-btn";
+  restBtn.innerHTML = "<img src='../static/images/plus.svg' alt='' />Отдых";
+  buttonsWrapper.appendChild(restBtn);
+
+  const exerciseBtn = document.createElement("button");
+  exerciseBtn.className = "white-btn";
+  exerciseBtn.onclick = () => showPage("workout-page");
+  exerciseBtn.innerHTML =
+    "<img src='../static/images/plus.svg' alt='' />Упражнение";
+  buttonsWrapper.appendChild(exerciseBtn);
+
+  restBtn.onclick = () => {
+    let lastExercise = customExercises[customExercises.length - 1];
+    if (lastExercise && lastExercise.rest_timeout > 0) {
+      lastExercise.rest_timeout += 10;
+    } else {
+      lastExercise.rest_timeout = 10;
+    }
+    customExercises.pop();
+    customExercises.push(lastExercise);
+    updateUI([{ type: "attributes", attributeName: "style" }]);
+  };
+
+  return buttonsWrapper;
+}
 
 function showPage(page) {
   const editWorkoutPage = document.getElementById("edit-workout-page");
@@ -151,7 +207,6 @@ function showPage(page) {
 }
 
 function addNewExercise(exercise) {
-  console.log(exercise);
   customExercises.push(exercise);
   showPage("edit-workout-page");
 }
