fef03415fd2eedd1d1b00c601e4042f1eeb67baa
Sirshak Bohara <boharasirshak101@gmail.com>
2024-06-11 14:16:22 +0545

    feat: buffertob64 converter

--- server/app/static/js/lib.js
+++ server/app/static/js/lib.js
@@ -117,3 +117,13 @@ function getCookie(cName) {
   });
   return res;
 }
+
+function bufferToBase64(buffer) {
+  let binary = "";
+  const bytes = new Uint8Array(buffer);
+  const len = bytes.byteLength;
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return window.btoa(binary);
+}
