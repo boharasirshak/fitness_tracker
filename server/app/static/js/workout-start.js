3d140fe072dd047c02e1494158de8f4a6e78541e
Sirshak Bohara <boharasirshak101@gmail.com>
2024-06-23 15:40:10 +0545

    feat: save session on every repetition cap

--- server/app/static/js/workout-start.js
+++ server/app/static/js/workout-start.js
@@ -15,6 +15,7 @@ const video = document.getElementById("video");
 const helperVideo = document.getElementById("helper-video");
 const repetitionsCountElement = document.getElementById("repetition-count");
 const timerElement = document.getElementById("timer");
+const completeButton = document.getElementById("complete-btn");
 const downloadButtonElement = document.getElementById("download-button");
 const protocol = window.location.protocol === "https:" ? "wss" : "ws";
 const ws = new WebSocket(`${protocol}://${window.location.host}/api/v1/ws`);
@@ -22,13 +23,22 @@ const accessToken = getCookie("access_token");
 
 let currentExercise;
 let currentExerciseIdx = 0;
-let exercises = [];
+let exercises = exercise ? [exercise] : workout.exercises;
 
-// TODO: add a case for single exercise
+if (exercises.length > 0) {
+  initializeExercise(exercises[currentExerciseIdx]);
+}
 
-if (exercise === null) {
-  exercises = workout.exercises;
-  currentExercise = exercises[currentExerciseIdx];
+initializeVideoStream();
+
+ws.onmessage = handleWebSocketMessage;
+
+video.addEventListener("play", startVideoProcessing);
+
+completeButton.addEventListener("click", completeWorkout);
+
+function initializeExercise(exercise) {
+  currentExercise = exercise;
   restTimer = currentExercise.rest_time;
   helperVideo.src = `../../static/videos/${currentExercise.video_link}`;
   document.querySelectorAll("[type='exercise-name']").forEach((el) => {
@@ -36,37 +46,27 @@ if (exercise === null) {
   });
 }
 
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then((stream) => {
-      video.srcObject = stream;
-    })
-    .catch(() => {
-      iziToast.show({
-        color: "red",
-        position: "topRight",
-        message:
-          "Пожалуйста, разрешите доступ к камере и микрофону, чтобы начать тренировку",
-        timeout: 5000,
+function initializeVideoStream() {
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        video.srcObject = stream;
+      })
+      .catch(() => {
+        showToast(
+          "red",
+          "Пожалуйста, разрешите доступ к камере и микрофону, чтобы начать тренировку"
+        );
+        redirectToWorkouts();
       });
-      setTimeout(() => {
-        window.location.href = "/workouts";
-      }, 5000);
-    });
-} else {
-  iziToast.show({
-    color: "red",
-    position: "topRight",
-    message: "Camers access is not supported in this browser",
-    timeout: 5000,
-  });
-  setTimeout(() => {
-    window.location.href = "/workouts";
-  }, 5000);
+  } else {
+    showToast("red", "Camera access is not supported in this browser");
+    redirectToWorkouts();
+  }
 }
 
-ws.onmessage = function (event) {
+function handleWebSocketMessage(event) {
   const message = JSON.parse(event.data);
 
   if (message.type === "count") {
@@ -74,60 +74,56 @@ ws.onmessage = function (event) {
     repetitionsCountElement.innerText = repetitions;
   }
   connectionId = message.connection_id;
-};
+}
 
-video.addEventListener("play", () => {
+function startVideoProcessing() {
   startTime = Date.now();
   const canvas = document.createElement("canvas");
   const context = canvas.getContext("2d");
 
-  const sendFrame = () => {
-    if (video.paused || video.ended) {
-      console.log("Video paused, ended.");
-      return;
-    }
+  function sendFrame() {
+    if (video.paused || video.ended) return;
 
     const aspectRatio = video.videoWidth / video.videoHeight;
-    let targetHeight = 240;
-    let targetWidth = aspectRatio * targetHeight;
+    const targetHeight = 240;
+    const targetWidth = aspectRatio * targetHeight;
 
     canvas.width = targetWidth;
     canvas.height = targetHeight;
-
     context.drawImage(video, 0, 0, targetWidth, targetHeight);
+
     canvas.toBlob(
       (blob) => {
-        blob.arrayBuffer().then((buffer) => {
-          let b64Data = isResting ? null : bufferToBase64(buffer);
-          const data = JSON.stringify({
-            type: currentExercise.exercise_id,
-            data: b64Data,
-            is_resting: isResting,
-            is_downloading: isDownloading,
-            is_completed: isCompleted,
+        if (blob) {
+          blob.arrayBuffer().then((buffer) => {
+            let b64Data = isResting ? null : bufferToBase64(buffer);
+            const data = JSON.stringify({
+              type: currentExercise.exercise_id,
+              data: b64Data,
+              is_resting: isResting,
+              is_downloading: isDownloading,
+              is_completed: isCompleted,
+            });
+            ws.send(data);
           });
-          ws.send(data);
-        });
+        }
       },
       "image/jpeg",
       0.9
     );
-    setTimeout(sendFrame, interval);
-  };
 
-  setInterval(() => {
-    if (!isDownloading) {
-      updateTimer();
-    }
-  }, 1000);
+    setTimeout(sendFrame, interval);
+  }
+
+  setInterval(updateTimer, 1000);
 
   sendFrame();
-});
+}
 
-document.getElementById("complete-btn").addEventListener("click", async () => {
+async function completeWorkout() {
   isDownloading = true;
-  document.getElementById("complete-btn").disabled = true;
-  document.getElementById("complete-btn").innerText = "Скачивание...";
+  completeButton.disabled = true;
+  completeButton.innerText = "Скачивание...";
 
   try {
     const response = await fetch(
@@ -143,76 +139,41 @@ document.getElementById("complete-btn").addEventListener("click", async () => {
 
     if (!response.ok) {
       const data = await response.json();
-      iziToast.show({
-        color: "red",
-        position: "topRight",
-        message: data.detail,
-        timeout: 3000,
-      });
-      document.getElementById("complete-btn").disabled = false;
-      document.getElementById("complete-btn").innerText = "Завершить";
-      isDownloading = false;
+      showToast("red", data.detail);
+      resetCompleteButton();
       return;
     }
 
     const blob = await response.blob();
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "video.mp4";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
+    downloadBlob(blob, "video.mp4");
   } catch (e) {
     console.error("Failed to download the video", e);
-    iziToast.show({
-      color: "red",
-      position: "topRight",
-      message: "Could not save the video. Please try again later.",
-      timeout: 3000,
-    });
+    showToast("red", "Could not save the video. Please try again later.");
   } finally {
-    document.getElementById("complete-btn").disabled = false;
-    document.getElementById("complete-btn").innerText = "Завершить";
-    isDownloading = false;
+    resetCompleteButton();
   }
 
-  const data = JSON.stringify({
-    type: "reset",
-    connection_id: connectionId,
-  });
-  ws.send(data);
+  ws.send(JSON.stringify({ type: "reset", connection_id: connectionId }));
   endTime = Date.now();
   endRestPeriod();
-});
+}
 
 function updateTimer() {
-  if (isResting) {
-    return;
-  }
+  if (isResting || isDownloading || !startTime) return;
 
-  if (isDownloading) {
-    return;
-  }
+  const totalTimeSpentSeconds = parseInt((Date.now() - startTime) / 1000);
+  const displaySeconds = totalTimeSpentSeconds % 60;
 
-  let totalTimeSpentSeconds = parseInt((Date.now() - startTime) / 1000);
-  let seconds = 0;
-
-  if (totalTimeSpentSeconds > 0 && totalTimeSpentSeconds % 60 === 0) {
+  if (totalTimeSpentSeconds % 60 === 0 && totalTimeSpentSeconds > 0) {
     seconds = 0;
     minutes++;
-  } else {
-    seconds = Math.round(totalTimeSpentSeconds % 60);
   }
 
-  timerElement.innerText = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  timerElement.innerText = `${minutes}:${
+    displaySeconds < 10 ? "0" : ""
+  }${displaySeconds}`;
 
-  if (startTime === undefined) {
-    return;
-  }
-
-  if (totalTimeSpentSeconds >= currentExercise.total_time && !isResting) {
+  if (repetitions >= currentExercise.repetitions && !isResting) {
     endTime = Date.now();
     saveSession();
     startRestPeriod();
@@ -230,16 +191,13 @@ function saveSession() {
       workout_id: workout.id,
       repetitions: repetitions,
       workout_exercise_id: currentExercise.id,
+      started_at: new Date(startTime).toISOString(),
+      finished_at: new Date(endTime).toISOString(),
     }),
   })
     .then((res) => {
       if (res.status !== 200) {
-        iziToast.show({
-          color: "red",
-          position: "topRight",
-          message: "Failed to save the exercise session",
-          timeout: 1000,
-        });
+        showToast("red", "Failed to save the exercise session");
       }
     })
     .catch((err) => {
@@ -250,7 +208,7 @@ function saveSession() {
 function startRestPeriod() {
   isResting = true;
   document.getElementById("time-label").innerText = "Отдых";
-  document.getElementById("helper-video").src = "";
+  helperVideo.src = "";
   updateRestTimer();
 }
 
@@ -261,11 +219,8 @@ function updateRestTimer() {
       restTimer--;
       updateRestTimer();
     }, 1000);
-  } else {
-    // to prevent condition where the user completes the exercise before the rest period ends
-    if (!isDownloading) {
-      endRestPeriod();
-    }
+  } else if (!isDownloading) {
+    endRestPeriod();
   }
 }
 
@@ -275,32 +230,58 @@ function endRestPeriod() {
   startTime = Date.now();
   currentExerciseIdx++;
 
-  if (exercises.length >= currentExerciseIdx + 1) {
-    currentExercise = exercises[currentExerciseIdx];
-    restTimer = currentExercise.rest_time;
-    helperVideo.src = `../../static/videos/${currentExercise.video_link}`;
-    restTimer = currentExercise.rest_time;
+  if (currentExerciseIdx < exercises.length) {
+    initializeExercise(exercises[currentExerciseIdx]);
     repetitions = 0;
-    ws.send(
-      JSON.stringify({
-        type: "reset",
-        connection_id: connectionId,
-      })
-    );
-    document.querySelectorAll("[type='exercise-name']").forEach((el) => {
-      el.innerText = currentExercise.name;
-    });
-    document.getElementById("helper-video").play();
+    ws.send(JSON.stringify({ type: "reset", connection_id: connectionId }));
+    helperVideo.play();
   } else {
     isCompleted = true;
-    iziToast.show({
-      color: "green",
-      position: "topRight",
-      message: "Тренировка завершена",
-      timeout: 5000,
-    });
+    showToast("green", "Тренировка завершена");
     setTimeout(() => {
       window.location.href = "/dashboard";
     }, 5000);
   }
+}
+
+function bufferToBase64(buffer) {
+  let binary = "";
+  let bytes = new Uint8Array(buffer);
+  let len = bytes.byteLength;
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return window.btoa(binary);
+}
+
+function showToast(color, message) {
+  iziToast.show({
+    color: color,
+    position: "topRight",
+    message: message,
+    timeout: 5000,
+  });
+}
+
+function redirectToWorkouts() {
+  setTimeout(() => {
+    window.location.href = "/workouts";
+  }, 5000);
+}
+
+function downloadBlob(blob, filename) {
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  window.URL.revokeObjectURL(url);
+}
+
+function resetCompleteButton() {
+  completeButton.disabled = false;
+  completeButton.innerText = "Завершить";
+  isDownloading = false;
 }
